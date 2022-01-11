e51c6b18193d594b199d61d48ed8421efbab0caa
monikkacha <monikkacha@gmail.com>
2022-01-11 12:52:10 +0530

    username password added

--- lib/widgets/home.dart
+++ lib/widgets/home.dart
@@ -11,8 +11,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Login"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Column(
+          children:[
+            SizedBox(height:50),
+            Image.asset('assets/image/abc.jpg'),
+            SizedBox(height:50),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'username',
+
+              ),
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'password',
+              ),
+            ),
+            SizedBox(height: 40 ),
+            ElevatedButton(
+              onPressed: () {
+                print("me clicked");
+              },
+              child: Text("login"),
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size.fromHeight(40)
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
